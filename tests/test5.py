fef6fe0acd9348d0c2bc35eeb6ab32c125838c1a
Mehmet Çalışkan <49614175+alkanchosen@users.noreply.github.com>
2022-04-25 18:00:20 +0300

    Improved test cases, added a new one

--- tests/test5.py
+++ tests/test5.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+
 # Test Case Description --> User allows geolocation and clicks get location button
 s = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
@@ -20,18 +21,18 @@ driver.get("https://cs458pno3.netlify.app/")
 driver.find_element(By.ID, "getLocation").click()
 
 try:
-    initial_country_text = "The country name is: Searching..."
-
-    WebDriverWait(driver, 20).until(
-        EC.text_to_be_present_in_element((By.ID, "distanceMoon"), "km")
-    )
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "toast-message")))
 
     assert (
             "km" in driver.find_element(By.ID, "distancePole").text
             and "km" in driver.find_element(By.ID, "distanceMoon").text
-            and driver.find_element(By.ID, "countryName").text
-            != initial_country_text
+            and driver.find_element(By.ID, "countryName").text != "The country name is: "
+            and driver.find_element(By.CLASS_NAME, "toast-message").text == "Successfully calculated!"
     )
     print("Test is passed")
-except Exception:
+except AssertionError:
     print("Test is failed")
+
+driver.close()
+
+
--- tests/test6.py
+++ tests/test6.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -6,28 +8,33 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# Test Case Description --> User enters valid latitude and longitude
+# Test Case Description --> User allows geolocation and clicks get location button but moon API is unreachable
 s = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_experimental_option(
+    "prefs", {"profile.default_content_setting_values.geolocation": 1}
+)
 driver = webdriver.Chrome(options=options, service=s)
+driver.execute_cdp_cmd('Network.setBlockedURLs', {"urls": ["https://moon-distance-api.herokuapp.com"]})
+driver.execute_cdp_cmd('Network.enable', {})
 driver.maximize_window()
 driver.get("https://cs458pno3.netlify.app/")
-driver.find_element(By.ID, "latitude").send_keys("40.730610")
-driver.find_element(By.ID, "longitude").send_keys("-73.935242")
-driver.find_element(By.ID, "submitLocation").click()
+driver.find_element(By.ID, "getLocation").click()
 
 try:
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "toast-message")))
 
     assert (
             "km" in driver.find_element(By.ID, "distancePole").text
-            and "km" in driver.find_element(By.ID, "distanceMoon").text
-            and "United States" in driver.find_element(By.ID, "countryName").text
-            and driver.find_element(By.CLASS_NAME, "toast-message").text == "Successfully calculated!"
+            and "Unknown" in driver.find_element(By.ID, "distanceMoon").text
+            and driver.find_element(By.ID, "countryName").text != "The country name is: "
+            and driver.find_element(By.CLASS_NAME, "toast-message").text == "Failed to calculate the distance to the moon's core!"
     )
     print("Test is passed")
 except AssertionError:
     print("Test is failed")
 
 driver.close()
+
+
